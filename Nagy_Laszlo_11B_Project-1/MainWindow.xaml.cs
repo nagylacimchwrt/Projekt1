f4538ef8527233144fdb548324809a4db2a595b0
nagylacimchwrt <92784296+nagylacimchwrt@users.noreply.github.com>
2021-12-12 15:32:15 +0100

    Iskolában történtek

---
+++ Nagy_Laszlo_11B_Project-1/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nagy_Laszlo_11B_Project_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<CheckBox> feladatok = new List<CheckBox>();
+        List<CheckBox> toroltek = new List<CheckBox>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            feladatokListaja.ItemsSource = feladatok;
+            toroltekListaja.ItemsSource = toroltek;
+        }
+
+        private void ujElemHozzadasa_Click(object sender, RoutedEventArgs e)
+        {
+            if (beSzoveg.Text != "")
+            {
+                CheckBox uj = new CheckBox();
+                uj.Content = beSzoveg.Text;
+                uj.Checked += new RoutedEventHandler(CheckBox_Checked);
+                uj.Unchecked += new RoutedEventHandler(CheckBox_Unchecked);
+                feladatok.Add(uj);
+                feladatokListaja.Items.Refresh();
+            }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox aktualis = (CheckBox)sender;
+            aktualis.FontStyle = FontStyles.Italic;
+            aktualis.Foreground = Brushes.Gray;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox aktualis = (CheckBox)sender;
+            aktualis.FontStyle = FontStyles.Normal;
+            aktualis.Foreground = Brushes.Black;
+        }
+
+        private void torlesGomb_Click(object sender, RoutedEventArgs e)
+        {
+            CheckBox kijelolt = (CheckBox)feladatokListaja.SelectedItem;
+            toroltek.Add(kijelolt);
+            feladatok.Remove(kijelolt);
+            feladatokListaja.Items.Refresh();
+            toroltekListaja.Items.Refresh();
+            asd
+        }
+    }
+}
